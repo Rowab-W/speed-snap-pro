714e0d151fd8a53c913a5ea859d3c1bd2532da47
Rowab-W <80970862+Rowab-W@users.noreply.github.com>
2025-08-17 16:15:45 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { createHtmlPlugin } from "vite-plugin-html";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -12,6 +13,14 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          csp: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
+        }
+      }
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
